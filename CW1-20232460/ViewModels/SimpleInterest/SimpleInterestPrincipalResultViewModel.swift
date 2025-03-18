1b4b6870059cb15b69eb5094ffa85e9863c6640c
Anuradha <anusampath9470@gmail.com>
2025-03-19 01:03:07 +0530

    changes

---
+++ CW1-20232460/ViewModels/SimpleInterest/SimpleInterestPrincipalResultViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  SimpleInterestPrincipalResultViewModel.swift
+//  CW1-20232460
+//
+//  Created by Anuradha Hewa Siribaddana on 2025-03-17.
+//
+
+import Foundation
+
+class SimpleInterestPrincipalResultViewModel: ObservableObject {
+    private let calculationService: ICalculationService
+    
+    init(calculationService: ICalculationService) {
+        self.calculationService = calculationService
+    }
+    
+    @Published var initialInvestmentAnswer: Double = 0
+    @Published var alertDetails = AlertDetails()
+    
+    private func showMessage(alertKey: String, message: String) {
+        alertDetails = AlertDetails(isPresented: true, message: message, alertKey: alertKey)
+    }
+    
+    func calculateResult(futureValue: String, interestRate: String, durationInYears: String) {
+        guard let t = Double(durationInYears),
+              let f = Double(futureValue),
+              let r = Double(interestRate)
+        else {
+            showMessage(alertKey: "Invalid input!", message: "Please enter valid numbers.")
+            return
+        }
+        
+        initialInvestmentAnswer = calculationService.getInitialInvestment(futureValue: f, periodInYears: t, interestRate: r)
+    }
+    
+    func resetModel() {
+        initialInvestmentAnswer = 0
+    }
+}
